14719bd774f3442fbd08fd5c550c0370d1877ef0
Adam Nowicki <adamnow83@gmail.com>
2023-06-19 16:14:10 +0200

    declared classes, not methods yet

---
+++ CC_PA/Clothes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC_PA
+{
+    public class Clothes
+    {
+        public int ClothId;
+        public string BrandName;
+        public string ClothType;
+
+        public Clothes(int clothID, string brandName, string clothType)
+        {
+            ClothId = clothID;
+            BrandName = brandName;
+            setClothType(clothType);
+        }
+        private void setClothType(string type)
+        {
+            clothTypes parsedType;
+            if(Enum.TryParse(type, out parsedType))
+            {
+                ClothType = parsedType.ToString();
+            }
+            else
+            {
+                ClothType = "wrong";
+            }
+
+        }
+    }
+    enum clothTypes
+    {
+        shirt,
+        blouse,
+        trousers,
+        skirt
+    }
+}
---
+++ CC_PA/Wardrobe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC_PA
+{
+    public class Wardrobe
+    {
+        public int Capacity;
+        public List<Hanger> ListOfHangers;
+
+        public Wardrobe(int capacity)
+        {
+            capacity = Capacity;
+        }
+    }
+}
